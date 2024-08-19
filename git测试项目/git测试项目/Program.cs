3f37c2adc98710e2af91edf343cab582a0f7c9e9
zccc <3492912044@qq.com>
2024-08-19 19:50:32 +0800

    添加一个Text文本

--- git测试项目/git测试项目/Program.cs
+++ git测试项目/git测试项目/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("program.cs添加了输出语句");
         }
         public int Sum(int a,int b)
         {
