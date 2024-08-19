94fdc2ed0b058d5d30588dfbcd8a2cdd098ba3ab
zccc <3492912044@qq.com>
2024-08-19 19:17:07 +0800

    添加一个加法

---
+++ git测试项目/git测试项目/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace git测试项目
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int Sum(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
